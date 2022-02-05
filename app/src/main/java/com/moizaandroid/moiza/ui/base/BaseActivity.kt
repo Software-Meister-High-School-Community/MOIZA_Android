f1a5cbf2e9624940a125881eed39bc677d85c5a2
saehyun <sh00710000@gmail.com>
2022-02-05 17:29:27 +0900

    ⚡️ :: base activity abstract fun

--- app/src/main/java/com/moizaandroid/moiza/ui/base/BaseActivity.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/base/BaseActivity.kt
@@ -1,23 +1,31 @@
 package com.moizaandroid.moiza.ui.base
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutResId : Int) : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutResId : Int
+    ) : AppCompatActivity() {
+
     protected lateinit var binding : T
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,layoutResId)
-        init()
+
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+
+        initView()
+        observeEvent()
     }
 
-    abstract fun init()
+    abstract fun initView()
+
+    abstract fun observeEvent()
 
     protected fun shortToast(msg : String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
@@ -26,5 +34,4 @@ abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutRe
     protected fun longToast(msg : String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
-
 }
