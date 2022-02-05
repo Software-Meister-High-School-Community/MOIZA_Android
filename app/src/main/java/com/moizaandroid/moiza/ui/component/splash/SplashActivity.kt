553bb35895ad88edfee14c2fa605eadbaad0511a
saehyun <sh00710000@gmail.com>
2022-02-05 19:15:53 +0900

    📝 :: Splash 생성

---
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.moizaandroid.moiza.ui.component.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moizaandroid.moiza.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
