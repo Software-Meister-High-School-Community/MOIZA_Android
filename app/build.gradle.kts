1a132c51211109709ad4d11b48ce7b637c2197dc
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-02-06 23:03:22 +0900

    📑 카테고리 UI 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("dagger.hilt.android.plugin")
     kotlin("android")
     kotlin("kapt")
+    id("kotlin-android")
 }
 
 android {
---
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/category/CategoryFragment.kt
@@ -0,0 +1,10 @@
+package com.moizaandroid.moiza.ui.component.category
+
+import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.databinding.FragmentCategoryBinding
+import com.moizaandroid.moiza.ui.base.BaseFragment
+
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
+    override fun init() {
+    }
+}
