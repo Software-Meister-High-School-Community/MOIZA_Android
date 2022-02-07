e02c03c6a963ff5b44a6080d48b876d8526fe491
임세현 <sh00710000@gmail.com>
2022-02-07 16:10:43 +0900

    📑 :: sign up activity

---
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/register/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.moizaandroid.moiza.ui.component.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moizaandroid.moiza.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
