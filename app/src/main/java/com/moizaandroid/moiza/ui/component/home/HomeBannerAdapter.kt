b0687f9928d4a69c4a005ae7ea452041f39703ac
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-02-22 22:00:31 +0900

    📝 :: add layout name

--- app/src/main/java/com/moizaandroid/moiza/ui/component/home/HomeBannerAdapter.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/home/HomeBannerAdapter.kt
@@ -1,12 +1,25 @@
 package com.moizaandroid.moiza.ui.component.home
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager.widget.PagerAdapter
+import com.moizaandroid.moiza.R
 
 class HomeBannerAdapter : PagerAdapter() {
+
+    val layout = arrayOf(
+        R.layout.banner_gsm_item,
+        R.layout.banner_dgsm_item,
+        R.layout.banner_dsm_item,
+        R.layout.banner_mirim_item,
+        R.layout.banner_bsm_item
+    )
+
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        return super.instantiateItem(container, position)
+        val view =
+            LayoutInflater.from(container.context).inflate(layout[position], container, false)
+        return view
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
