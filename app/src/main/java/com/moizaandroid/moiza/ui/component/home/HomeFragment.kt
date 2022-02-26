7ce918eeea408e0469674962212463b1eebad5f8
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-02-26 23:14:18 +0900

    📝 :: change search view

--- app/src/main/java/com/moizaandroid/moiza/ui/component/home/HomeFragment.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/home/HomeFragment.kt
@@ -1,16 +1,18 @@
 package com.moizaandroid.moiza.ui.component.home
 
 
+import android.graphics.Color
+import android.text.Html
+import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
 import com.moizaandroid.moiza.R
 import com.moizaandroid.moiza.databinding.FragmentHomeBinding
 import com.moizaandroid.moiza.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
     override fun init() {
-        initSchoolBanner()
+
     }
 
-    private fun initSchoolBanner(){
-        binding.schoolBanner.adapter = HomeBannerAdapter()
-    }
 }
