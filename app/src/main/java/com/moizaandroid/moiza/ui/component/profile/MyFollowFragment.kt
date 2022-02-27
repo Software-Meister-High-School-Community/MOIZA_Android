9aa8593d3559ee22c25145aeca0e9de5d231d4a6
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-02-27 23:21:57 +0900

    📝 :: change edit/follow view

---
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/profile/MyFollowFragment.kt
@@ -0,0 +1,12 @@
+package com.moizaandroid.moiza.ui.component.profile
+
+import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.databinding.FragmentProfileMyfollowBinding
+import com.moizaandroid.moiza.ui.base.BaseFragment
+
+class MyFollowFragment :
+    BaseFragment<FragmentProfileMyfollowBinding>(R.layout.fragment_profile_myfollow) {
+    override fun init() {
+
+    }
+}
