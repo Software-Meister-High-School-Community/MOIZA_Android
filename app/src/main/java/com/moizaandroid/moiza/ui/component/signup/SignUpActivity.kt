4700f721ec832a85eb47eee718b32610838d9367
임세현 <sh00710000@gmail.com>
2022-02-28 20:18:12 +0900

    ⚡️ :: setup base

--- app/src/main/java/com/moizaandroid/moiza/ui/component/signup/SignUpActivity.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/signup/SignUpActivity.kt
@@ -3,10 +3,21 @@ package com.moizaandroid.moiza.ui.component.signup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.databinding.ActivitySignUpBinding
+import com.moizaandroid.moiza.ui.base.BaseActivity
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity<ActivitySignUpBinding> (
+    R.layout.activity_sign_up
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
     }
 }
