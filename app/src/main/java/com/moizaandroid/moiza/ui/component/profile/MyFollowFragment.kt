7b95f979f740fadda472ac82f8aa5cd8c461b769
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-02 09:02:43 +0900

    ⚡️ :: back btn

--- app/src/main/java/com/moizaandroid/moiza/ui/component/profile/MyFollowFragment.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/profile/MyFollowFragment.kt
@@ -1,5 +1,7 @@
 package com.moizaandroid.moiza.ui.component.profile
 
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import com.moizaandroid.moiza.R
 import com.moizaandroid.moiza.databinding.FragmentProfileMyfollowBinding
 import com.moizaandroid.moiza.ui.base.BaseFragment
@@ -7,6 +9,12 @@ import com.moizaandroid.moiza.ui.base.BaseFragment
 class MyFollowFragment :
     BaseFragment<FragmentProfileMyfollowBinding>(R.layout.fragment_profile_myfollow) {
     override fun init() {
+        clickBackBtn()
+    }
 
+    private fun clickBackBtn(){
+        binding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
--- app/src/main/java/com/moizaandroid/moiza/ui/component/search/SearchFragment.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.moizaandroid.moiza.ui.component.search
 
 
+import androidx.navigation.fragment.findNavController
 import com.moizaandroid.moiza.R
 import com.moizaandroid.moiza.databinding.FragmentSearchBinding
 import com.moizaandroid.moiza.ui.base.BaseFragment
@@ -8,7 +9,12 @@ import com.moizaandroid.moiza.ui.base.BaseFragment
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun init() {
-
+        clickBackBtn()
     }
 
+    private fun clickBackBtn(){
+        binding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
