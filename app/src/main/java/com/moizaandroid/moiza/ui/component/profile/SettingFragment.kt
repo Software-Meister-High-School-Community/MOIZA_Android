ce4bfcaede57a18e7fe482d86eac42425999a275
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-02 09:25:10 +0900

    📝 :: add settinglayout

---
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/profile/SettingFragment.kt
@@ -0,0 +1,11 @@
+package com.moizaandroid.moiza.ui.component.profile
+
+import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.databinding.FragmentProfileSettingBinding
+import com.moizaandroid.moiza.ui.base.BaseFragment
+
+class SettingFragment: BaseFragment<FragmentProfileSettingBinding>(R.layout.fragment_profile_setting) {
+    override fun init() {
+
+    }
+}
