3adf9711e7800737720ab386084fd93d2c118837
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-03-04 07:52:19 +0900

    📝 :: update layout

--- app/src/main/java/com/moizaandroid/moiza/ui/component/home/BannerFragment.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/home/BannerFragment.kt
@@ -1,11 +1,11 @@
 package com.moizaandroid.moiza.ui.component.home
 
 import android.graphics.Color
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.text.Html
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.marginStart
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager.widget.ViewPager
 import com.moizaandroid.moiza.R
@@ -15,6 +15,18 @@ import com.moizaandroid.moiza.ui.base.BaseFragment
 class BannerFragment : BaseFragment<FragmentHomeBannerBinding>(R.layout.fragment_home_banner) {
 
     var dots: Array<TextView?> = emptyArray()
+    var time: Long = 1500
+    private val Handler = HomeBannerHandler()
+
+    override fun onResume() {
+        super.onResume()
+        autoScroll(time)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        autoScrollStop()
+    }
 
     override fun init() {
         initSchoolBanner()
@@ -42,7 +54,7 @@ class BannerFragment : BaseFragment<FragmentHomeBannerBinding>(R.layout.fragment
             dots[i] = TextView(context)
             dots[i]?.setText(Html.fromHtml("&#9679;"))
             dots[i]?.setTextSize(8f)
-            dots[i]?.setPadding(5,0,5,0)
+            dots[i]?.setPadding(5, 0, 5, 0)
             if (i == 0) {
                 dots[i]?.setTextColor(Color.parseColor("#555555"))
             } else {
@@ -71,14 +83,39 @@ class BannerFragment : BaseFragment<FragmentHomeBannerBinding>(R.layout.fragment
             }
 
             override fun onPageScrollStateChanged(state: Int) {
+                when (state) {
+                    ViewPager.SCROLL_STATE_DRAGGING -> autoScrollStop()
+                    ViewPager.SCROLL_STATE_IDLE -> autoScroll(time)
+                }
             }
         })
     }
 
-    fun clickSearchBtn() {
+    private fun clickSearchBtn() {
         binding.searchTabBtn.setOnClickListener {
             this.findNavController().navigate(R.id.action_bannerFragment_to_searchFragment)
         }
     }
 
+    private fun autoScroll(time: Long) {
+        Handler.removeMessages(0)
+        Handler.sendEmptyMessageDelayed(0, time)
+    }
+
+    private fun autoScrollStop() {
+        Handler.removeMessages(0)
+    }
+
+    private inner class HomeBannerHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if (msg.what == 0) {
+                binding.schoolBanner.setCurrentItem(
+                    if (binding.schoolBanner.currentItem >= 4) 0 else ++binding.schoolBanner.currentItem,
+                    true
+                )
+                autoScroll(time)
+            }
+        }
+    }
 }
