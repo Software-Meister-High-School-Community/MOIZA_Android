457d82494c45e1f43602ead9f2c6b82c994cb20b
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-03-05 18:02:02 +0900

    📑 Radius setting

--- app/src/main/java/com/moizaandroid/moiza/ui/component/post/PostFragment.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/component/post/PostFragment.kt
@@ -1,5 +1,6 @@
 package com.moizaandroid.moiza.ui.component.post
 
+import android.view.View
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.moizaandroid.moiza.R
@@ -7,13 +8,22 @@ import com.moizaandroid.moiza.databinding.FragmentPostBinding
 import com.moizaandroid.moiza.ui.base.BaseFragment
 import com.moizaandroid.viewmodel.MainViewModel
 
-class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post), View.OnClickListener {
 
     private val mainViewModel by activityViewModels<MainViewModel>()
 
     override fun init() {
         settingDialog()
         settingCategory()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.apply {
+            allPostBtn.setOnClickListener(this@PostFragment)
+            questionPostBtn.setOnClickListener(this@PostFragment)
+            normalPostBtn.setOnClickListener(this@PostFragment)
+        }
     }
 
 
@@ -31,5 +41,30 @@ class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
         }
     }
 
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.allPostBtn -> {
+                binding.apply{
+                    allPostBtn.isChecked = true
+                    questionPostBtn.isChecked = false
+                    normalPostBtn.isChecked = false
+                }
+            }
+            R.id.questionPostBtn -> {
+                binding.apply {
+                    allPostBtn.isChecked = false
+                    questionPostBtn.isChecked = true
+                    normalPostBtn.isChecked = false
+                }
+            }
+            R.id.normalPostBtn -> {
+                binding.apply {
+                    allPostBtn.isChecked = false
+                    questionPostBtn.isChecked = false
+                    normalPostBtn.isChecked = true
+                }
+            }
+        }
+    }
 
 }
