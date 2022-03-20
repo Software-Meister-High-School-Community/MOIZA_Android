db6185cbef790a47dd582399093e0a385f5eab67
임세현 <sh00710000@gmail.com>
2022-03-20 19:36:50 +0900

    ⚡️ :: typography

--- app/src/main/java/com/moizaandroid/moiza/ui/theme/Type.kt
+++ app/src/main/java/com/moizaandroid/moiza/ui/theme/Type.kt
@@ -2,14 +2,25 @@ package com.moizaandroid.moiza.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.moizaandroid.moiza.R
+
+val notoSansFamily = FontFamily(
+    Font(R.font.noto_sans_kr_black, FontWeight.Black),
+    Font(R.font.noto_sans_kr_bold, FontWeight.Bold),
+    Font(R.font.noto_sans_kr_medium, FontWeight.Medium),
+    Font(R.font.noto_sans_kr_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_kr_light, FontWeight.Light),
+    Font(R.font.noto_sans_kr_thin, FontWeight.Thin)
+)
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 14.sp
     )
 )
