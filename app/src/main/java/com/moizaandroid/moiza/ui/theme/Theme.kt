5ac748b71c7a898ccfbe8eb6c666f07b11f37894
임세현 <sh00710000@gmail.com>
2022-03-20 18:38:45 +0900

    ⚡️ :: compose theme

---
+++ app/src/main/java/com/moizaandroid/moiza/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.moizaandroid.moiza.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Yellow,
+    primaryVariant = Black,
+    secondary = Gray100
+)
+
+private val LightColorPalette = lightColors(
+    primary = Yellow,
+    primaryVariant = Black,
+    secondary = Gray100
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ComposeExample2Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
