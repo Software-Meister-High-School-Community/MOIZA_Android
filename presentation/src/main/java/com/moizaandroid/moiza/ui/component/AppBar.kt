f2b15530060ad2186ba17202bd0a869b65354f35
임세현 <sh00710000@gmail.com>
2022-05-10 23:39:17 +0900

    ⚡️ :: app bar modifier

--- presentation/src/main/java/com/moizaandroid/moiza/ui/component/AppBar.kt
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/component/AppBar.kt
@@ -6,64 +6,85 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.ui.theme.Body3
 import com.moizaandroid.moiza.ui.theme.Gray300
-import com.moizaandroid.moiza.ui.theme.Typography
 
 @Composable
-fun AppBar(text: String, onBackButtonClick: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Transparent)
+fun AppBar(
+    modifier: Modifier = Modifier,
+    text: String,
+    isSignUp: Boolean = false,
+    onBackButtonClick: () -> Unit
+) {
+    Surface(
+        modifier = modifier
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(56.dp)
                 .background(Color.Transparent)
         ) {
-            Image(
+            Box(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .size(24.dp)
-                    .clickable(
-                        interactionSource = MutableInteractionSource(),
-                        indication = null
-                    ) {
-                        onBackButtonClick()
-                    },
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = null
-            )
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .background(Color.Transparent)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(24.dp)
+                        .clickable(
+                            interactionSource = MutableInteractionSource(),
+                            indication = null
+                        ) {
+                            onBackButtonClick()
+                        },
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null
+                )
 
-            Text(
-                text = text,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .wrapContentWidth(Alignment.CenterHorizontally)
-                    .wrapContentHeight(Alignment.CenterVertically)
-                    .padding(start = 54.dp),
-                style = Typography.body1
+                Body3(
+                    text = text,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                        .wrapContentHeight(Alignment.CenterVertically)
+                        .padding(start = 54.dp),
+                )
+
+                if (isSignUp) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_moiza_logo),
+                        contentDescription = "moiza logo",
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                            .padding(end = 21.dp)
+                    )
+                }
+            }
+            Divider(
+                color = Gray300,
+                thickness = 1.dp
             )
         }
-        Divider(
-            color = Gray300,
-            thickness = 1.dp
-        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun AppBarPreview() {
-    AppBar(text = "로그인") {}
+    AppBar(text = "로그인", isSignUp = true, onBackButtonClick = {})
 }
