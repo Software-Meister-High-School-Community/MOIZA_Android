3082c77dabc504eb95e8d3fc579c65824546082e
임세현 <sh00710000@gmail.com>
2022-05-11 22:44:05 +0900

    📑 :: agreed

---
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/component/Agreed.kt
@@ -0,0 +1,60 @@
+package com.moizaandroid.moiza.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.moizaandroid.moiza.ui.theme.Body3
+import com.moizaandroid.moiza.ui.theme.Gray200
+import com.moizaandroid.moiza.ui.theme.Gray400
+
+@Composable
+fun Agreed(
+    text: String,
+    required: Boolean = false,
+    backgroundColor: Color = Color.White,
+    onValueChange: ((Boolean) -> Unit)?,
+    clickDetailBtn: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(color = backgroundColor)
+            .fillMaxWidth()
+    ) {
+        if (onValueChange != null) {
+            MoizaCheckBox(
+                text = if (required) "$text (필수)" else text,
+                onCheckOn = { onValueChange(true) },
+                onCheckOff = { onValueChange(false) },
+                modifier = Modifier.align(
+                    Alignment.CenterStart
+                ),
+                textSize = 14.sp,
+                checkBoxSize = 20.dp
+            )
+
+            Body3(
+                text = "보기",
+                color = Gray400,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .clickable { clickDetailBtn() })
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewAgreed() {
+    Agreed(text = "개인정보 취급 방침", onValueChange = {}, backgroundColor = Gray200)
+}
