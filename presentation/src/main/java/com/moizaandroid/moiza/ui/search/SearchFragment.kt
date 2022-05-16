0c3b094b3706fe1cc3922f866d70012016198b67
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-16 09:13:33 +0900

    ⚡️ :: move resultFragment

--- presentation/src/main/java/com/moizaandroid/moiza/ui/search/SearchFragment.kt
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/search/SearchFragment.kt
@@ -1,20 +1,29 @@
 package com.moizaandroid.moiza.ui.search
 
 
+import android.util.Log
 import androidx.navigation.fragment.findNavController
 import com.moizaandroid.moiza.R
 import com.moizaandroid.moiza.databinding.FragmentSearchBinding
 import com.moizaandroid.moiza.ui.base.BaseFragment
+import com.moizaandroid.moiza.ui.search.result.ResultSearchFragment
 
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun init() {
         clickBackBtn()
+        search()
     }
 
     private fun clickBackBtn(){
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun search(){
+        binding.searchBtn.setOnClickListener {
+            childFragmentManager.beginTransaction().replace(R.id.searchFrag, ResultSearchFragment()).commit()
         }
     }
 }
