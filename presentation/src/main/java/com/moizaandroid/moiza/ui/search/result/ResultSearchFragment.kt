aa13df1e582de6890ffd1842b74aef25eb93511b
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-05-16 20:53:00 +0900

    📑 :: resultFragment

---
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/search/result/ResultSearchFragment.kt
@@ -0,0 +1,12 @@
+package com.moizaandroid.moiza.ui.search.result
+
+import com.moizaandroid.moiza.R
+import com.moizaandroid.moiza.databinding.FragmentResultSearchBinding
+import com.moizaandroid.moiza.ui.base.BaseFragment
+
+class ResultSearchFragment: BaseFragment<FragmentResultSearchBinding>(R.layout.fragment_result_search) {
+    override fun init() {
+
+    }
+
+}
