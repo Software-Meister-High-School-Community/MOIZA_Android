cc5df6bc3e44840d6245ea71d7145253712a6644
임세현 <sh00710000@gmail.com>
2022-05-25 23:22:26 +0900

    ♻️ :: clean up sign up input student screen

--- presentation/src/main/java/com/moizaandroid/moiza/ui/register/SignUpInputStudentScreen.kt
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/register/SignUpInputStudentScreen.kt
@@ -28,6 +28,20 @@ fun SignUpInputStudentScreen(
 ) {
     val verticalScroll = rememberScrollState()
 
+    var isGraduated by remember { mutableStateOf<Boolean?>(null) }
+
+    var name by remember { mutableStateOf(String()) }
+
+    var isMale by remember { mutableStateOf(true) }
+
+    var birthDay by remember { mutableStateOf<Int?>(null) }
+
+    var school by remember { mutableStateOf<Schools?>(null) }
+
+    var userId by remember { mutableStateOf(String()) }
+
+    var verificationCode by remember { mutableStateOf(String()) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -61,182 +75,54 @@ fun SignUpInputStudentScreen(
 
             Spacer(modifier = Modifier.height(43.dp))
 
-            Body3(text = stringResource(id = R.string.sortation))
 
-            Spacer(modifier = Modifier.height(15.dp))
-
-            var graduate by remember { mutableStateOf(false) }
-            var ungraduate by remember { mutableStateOf(false) }
-
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                MoizaCheckBox(
-                    modifier = Modifier.fillMaxWidth(0.5f),
-                    text = stringResource(id = R.string.ungraduate),
-                    checked = ungraduate,
-                    onCheckedChange = {
-                        ungraduate = it
-                        graduate = !it
-                    }
-                )
-                MoizaCheckBox(
-                    text = stringResource(id = R.string.graduate),
-                    checked = graduate,
-                    onCheckedChange = {
-                        graduate = it
-                        ungraduate = !it
-                    }
-                )
-            }
-
-            Spacer(modifier = Modifier.height(50.dp))
-
-            Body3(text = stringResource(id = R.string.name))
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            var name by remember { mutableStateOf(String()) }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(44.dp)
-            ) {
-                MoizaTextField(
-                    value = name,
-                    onValueChange = { name = it },
-                    modifier = Modifier.fillMaxWidth(0.7f)
-                )
-
-                Spacer(modifier = Modifier.width(10.dp))
-
-                var isMale by remember { mutableStateOf(true) }
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(44.dp)
-                        .border(
-                            shape = RoundedCornerShape(topStart = 1.dp, bottomStart = 1.dp),
-                            width = 1.dp,
-                            color = Gray300
-                        )
-                        .background(color = if (isMale) Orange else Color.White)
-                        .clickable {
-                            isMale = !isMale
-                        }
-                ) {
-                    Body3(
-                        text = stringResource(id = R.string.male), modifier = Modifier
-                            .fillMaxSize()
-                            .wrapContentSize(Center)
-                    )
+            SignUpInputStudentGraduateCheckBox(
+                isGraduated = isGraduated,
+                doOnGraduateStudentClick = {
+                    isGraduated = true
+                },
+                doOnNotGraduateStudentClick = {
+                    isGraduated = false
                 }
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(44.dp)
-                        .border(
-                            shape = RoundedCornerShape(topEnd = 1.dp, bottomEnd = 1.dp),
-                            width = 1.dp,
-                            color = Gray300
-                        )
-
-                        .background(if (!isMale) Orange else Color.White)
-                        .clickable {
-                            isMale = !isMale
-                        }
-                ) {
-                    Body3(
-                        text = stringResource(id = R.string.female), modifier = Modifier
-                            .fillMaxSize()
-                            .wrapContentSize(Center)
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.height(50.dp))
-
-            Body3(text = stringResource(id = R.string.yyyyddmm))
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            var birthDay by remember { mutableStateOf(String()) }
-
-            MoizaTextField(value = birthDay, onValueChange = { birthDay = it }, keyboardType = KeyboardType.Number)
-
-            Spacer(modifier = Modifier.height(50.dp))
-
-            Body3(text = stringResource(id = R.string.select_school))
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            val schoolName = listOf(
-                Schools.BSM.schoolName,
-                Schools.DGSM.schoolName,
-                Schools.DSM.schoolName,
-                Schools.GSM.schoolName,
-                Schools.MIRIN.schoolName
             )
 
-            var school: String by remember { mutableStateOf(String())}
+            Spacer(modifier = Modifier.height(50.dp))
 
-            val schoolEmail = when (school) {
-                Schools.BSM.schoolName -> Schools.BSM.schoolEmail
-                Schools.DGSM.schoolName -> Schools.DGSM.schoolEmail
-                Schools.DSM.schoolName -> Schools.DSM.schoolEmail
-                Schools.GSM.schoolName -> Schools.GSM.schoolEmail
-                Schools.MIRIN.schoolName -> Schools.MIRIN.schoolEmail
-                else -> ""
-            }
+            SignUpInputStudentName(
+                name = name,
+                onNameChanged = { name = it },
+                isMale = isMale,
+                onSexChanged = { isMale = it }
+            )
 
-            MoizaVerticalMenus(onMenuClicked = { school = it }, menus = schoolName)
+            Spacer(modifier = Modifier.height(50.dp))
+
+            SignUpInputStudentBirthDay(
+                birthDay = birthDay,
+                onBirthDayChanged = { birthDay = it }
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            SignUpInputStudentSelectSchool(
+                school = school,
+                onSchoolChanged = { school = it }
+            )
 
             Spacer(modifier = Modifier.height(32.dp))
 
-            Body3(text = stringResource(id = R.string.school_email))
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            var userId by remember { mutableStateOf(String()) }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                MoizaTextField(
-                    value = userId,
-                    onValueChange = { userId = it },
-                    modifier = Modifier.fillMaxWidth(0.35f)
-                )
-
-                Spacer(modifier = Modifier.weight(0.5f))
-
-                Body3(text = "@ $schoolEmail")
-
-                Spacer(modifier = Modifier.weight(0.5f))
-
-                OutlinedButton(
-                    onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Gray200,
-                        contentColor = Gray600
-                    ),
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    Body3(text = stringResource(id = R.string.request_verity))
-                }
-            }
+            SignUpInputStudentSchoolEmail(
+                userId = userId,
+                onUserIdChanged = { userId = it },
+                school = school
+            )
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            Body3(text = stringResource(id = R.string.verification_code))
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            var verificationCode by remember { mutableStateOf(String()) }
-            MoizaTextField(value = verificationCode, onValueChange = { verificationCode = it })
+            SignUpInputStudentVerificationCode(
+                verificationCode = verificationCode,
+                onVerificationCodeChanged = { verificationCode = it }
+            )
 
             Spacer(Modifier.height(75.dp))
 
@@ -267,6 +153,183 @@ fun SignUpInputStudentScreen(
     }
 }
 
+@Composable
+fun SignUpInputStudentVerificationCode(
+    verificationCode: String,
+    onVerificationCodeChanged: (String) -> Unit
+) {
+    Body3(text = stringResource(id = R.string.verification_code))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    MoizaTextField(value = verificationCode, onValueChange = { onVerificationCodeChanged(it) })
+
+}
+@Composable
+fun SignUpInputStudentSchoolEmail(
+    userId: String,
+    onUserIdChanged: (String) -> Unit,
+    school: Schools?
+) {
+
+    Body3(text = stringResource(id = R.string.school_email))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        MoizaTextField(
+            value = userId,
+            onValueChange = { onUserIdChanged(it) },
+            modifier = Modifier.fillMaxWidth(0.35f)
+        )
+
+        Spacer(modifier = Modifier.weight(0.5f))
+
+        Body3(text = "@ ${school?.schoolEmail ?: ""}")
+
+        Spacer(modifier = Modifier.weight(0.5f))
+
+        OutlinedButton(
+            onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+                backgroundColor = Gray200,
+                contentColor = Gray600
+            ),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            Body3(text = stringResource(id = R.string.request_verity))
+        }
+    }
+}
+
+@Composable
+fun SignUpInputStudentSelectSchool(
+    school: Schools?,
+    onSchoolChanged: (Schools) -> Unit
+) {
+    Body3(text = stringResource(id = R.string.select_school))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    MoizaVerticalMenus(
+        onMenuClicked = { onSchoolChanged(it) },
+        menus = Schools.values().toList(),
+        selectedSchool = school?.schoolName ?: ""
+    )
+}
+@Composable
+fun SignUpInputStudentBirthDay(
+    birthDay: Int?,
+    onBirthDayChanged: (Int) -> Unit
+) {
+    Body3(text = stringResource(id = R.string.yyyyddmm))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    MoizaTextField(
+        value = birthDay?.toString() ?: "",
+        onValueChange = { onBirthDayChanged(it.toInt()) },
+        keyboardType = KeyboardType.Number
+    )
+
+}
+@Composable
+fun SignUpInputStudentGraduateCheckBox(
+    isGraduated: Boolean?,
+    doOnGraduateStudentClick: (Boolean) -> Unit,
+    doOnNotGraduateStudentClick: (Boolean) -> Unit
+) {
+    Body3(text = stringResource(id = R.string.sortation))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        MoizaCheckBox(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            text = stringResource(id = R.string.ungraduate),
+            checked = isGraduated == false,
+            onCheckedChange = doOnNotGraduateStudentClick
+        )
+        MoizaCheckBox(
+            text = stringResource(id = R.string.graduate),
+            checked = isGraduated == true,
+            onCheckedChange = doOnGraduateStudentClick
+        )
+    }
+}
+
+@Composable
+fun SignUpInputStudentName(
+    name: String,
+    onNameChanged: (String) -> Unit,
+    isMale: Boolean,
+    onSexChanged: (Boolean) -> Unit
+) {
+    Body3(text = stringResource(id = R.string.name))
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(44.dp)
+    ) {
+        MoizaTextField(
+            value = name,
+            onValueChange = { onNameChanged(it) },
+            modifier = Modifier.fillMaxWidth(0.7f)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(44.dp)
+                .border(
+                    shape = RoundedCornerShape(topStart = 1.dp, bottomStart = 1.dp),
+                    width = 1.dp,
+                    color = Gray300
+                )
+                .background(color = if (isMale) Orange else Color.White)
+                .clickable {
+                    onSexChanged(true)
+                }
+        ) {
+            Body3(
+                text = stringResource(id = R.string.male), modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Center)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(44.dp)
+                .border(
+                    shape = RoundedCornerShape(topEnd = 1.dp, bottomEnd = 1.dp),
+                    width = 1.dp,
+                    color = Gray300
+                )
+
+                .background(if (!isMale) Orange else Color.White)
+                .clickable {
+                    onSexChanged(false)
+                }
+        ) {
+            Body3(
+                text = stringResource(id = R.string.female), modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Center)
+            )
+        }
+    }
+}
 
 @Preview(
     showBackground = true,
