1bd3df92df8ab139adb7074e370671fe67bf9bfa
임세현 <sh00710000@gmail.com>
2022-06-05 18:49:53 +0900

    📑 :: add create post icons

--- moiza-design/src/main/java/com/moiza_design/icon/MoizaIcons.kt
+++ moiza-design/src/main/java/com/moiza_design/icon/MoizaIcons.kt
@@ -10,4 +10,7 @@ object MoizaIcons {
     val PasswordVisible = R.drawable.ic_pw_visible
     val PasswordInvisible = R.drawable.ic_pw_invisible
     val Camera = R.drawable.ic_all_camera
+    val CreateImage = R.drawable.ic_create_image
+    val WhitePlus = R.drawable.ic_plus_white
+    val WhiteClose = R.drawable.ic_close_white
 }
