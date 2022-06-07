4771be8d94ef4a25827bb123e1c5e95299715895
임세현 <sh00710000@gmail.com>
2022-06-07 22:42:52 +0900

    📑 :: reply post screen

---
+++ presentation/src/main/java/com/moizaandroid/moiza/ui/post/request/ReplyPostScreen.kt
@@ -0,0 +1,18 @@
+package com.moizaandroid.moiza.ui.post.request
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.moiza_design.theme.MoizaTheme
+
+@Composable
+fun ReplyPostScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewReplyPostScreen() {
+    MoizaTheme {
+        ReplyPostScreen()
+    }
+}
